35e6341aedec61cd4ab7001ec7c443f06de17c67
Matthew Rudy Jacobs <MatthewRudyJacobs@gmail.com>
2014-03-02 22:26:05 +0000

    introduce a bit of Cassandra

--- Gemfile
+++ Gemfile
@@ -4,3 +4,7 @@ source 'https://rubygems.org'
 gem 'sinatra'
 gem 'slim'
 gem 'puma'
+
+# cassandra
+gem 'cql-rb', '2.0.0.pre0'
+gem 'snappy'
--- app.rb
+++ app.rb
@@ -9,7 +9,29 @@ set :views, 'views'
 set :slim, pretty: true
 set :server, :puma
 
+# Set up a logger
+require 'logger'
+LOGGER = Logger.new($stderr)
+
+# Set up Cassandra
+require 'cql'
+require 'cql/compression/snappy_compressor'
+
+CASSANDRA_KEYSPACE = 'twissandra'
+
+CASSANDRA = Cql::Client.connect(
+  hosts: ['127.0.0.1'],
+  compressor: Cql::Compression::SnappyCompressor.new,
+  logger: LOGGER)
+
+CASSANDRA.use(CASSANDRA_KEYSPACE)
+
+def get_public_userline
+  CASSANDRA.execute("SELECT * FROM userline WHERE username=?", "!PUBLIC!")
+end
+
 # Application routes
 get '/' do
+  @tweets = get_public_userline
   slim :index
 end
